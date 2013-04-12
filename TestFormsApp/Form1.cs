4b4d1b4504aa7389d1de320d909300bb49407023
Super Cool Dude <wickedsideburns@gmail.com>
2013-04-11 20:34:46 -0700

    Initialization

---
+++ TestFormsApp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.NameTag.Enabled == false)
+            {
+                this.NameTag.Enabled = true;
+                this.NameText.Enabled = true;
+            }
+            else
+            {
+                this.NameTag.Enabled = false;
+                this.NameText.Enabled = false;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.NameTag.Enabled = false;
+            this.NameText.Enabled = false;
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            this.NameTag.Enabled = false;
+            this.NameTag.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string gerry;
+            gerry = MapInput.Text;
+            var formz = new Form2(gerry);
+            formz.Show();
+        }
+    }
+}
---
+++ TestFormsApp/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestFormsApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string s)
+        {
+            
+            InitializeComponent();
+            james = s;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            this.label1.Text = james;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string james;
+    }
+}
